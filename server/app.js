680ff3ca858ca8c779abbcd0fea2cbb7db94b400
koda-momo <momoko.kodama@rakus-partners.co.jp>
2022-02-21 10:27:28 +0900

    MogoDBに繋ぐ設定完了

--- server/app.js
+++ server/app.js
@@ -1,10 +1,20 @@
 const express = require("express");
 const app = express();
 const graphqlHTTP = require("express-graphql").graphqlHTTP;
+const mongoose = require("mongoose");
+const dbUrl = require("./.env.local");
+mongoose.connect(dbUrl);
 
 /**
- * ポート番号,起動した際に発動するメソッド
+ * ポート番号,起動した際に発動するメソッド.
  */
 app.listen(4000, () => {
   console.log("サーバに接続しました");
+});
+
+/**
+ * mongoDBに接続したら際に発動するメソッド.
+ */
+mongoose.connection.once("open", () => {
+  console.log("mongoDB接続完了");
 });
