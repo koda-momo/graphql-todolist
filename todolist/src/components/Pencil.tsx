a9606b83ef35c04f24ce66c4442260907709a23b
koda-momo <momoko.kodama@rakus-partners.co.jp>
2022-02-21 16:02:10 +0900

    ボタン追加

---
+++ todolist/src/components/Pencil.tsx
@@ -0,0 +1,27 @@
+import { FC, memo } from "react";
+
+export const Pencil: FC = memo(() => {
+  return (
+    <>
+      <button
+        type="button"
+        className="bg-yellow-400 w-10 h-10 flex items-center justify-center rounded-md text-white"
+      >
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          className="h-6 w-6"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke="currentColor"
+        >
+          <path
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            stroke-width="2"
+            d="M15.232 5.232l3.536 3.536m-2.036-5.036a2.5 2.5 0 113.536 3.536L6.5 21.036H3v-3.572L16.732 3.732z"
+          />
+        </svg>
+      </button>
+    </>
+  );
+});
--- todolist/src/components/TodoList.tsx
+++ todolist/src/components/TodoList.tsx
@@ -1,20 +1,24 @@
 import { FC, memo } from "react";
+import { TrashBox } from "./TrashBox";
+import { Pencil } from "./Pencil";
 
 export const TodoList: FC = memo(() => {
   return (
     <>
-      <div className="bg-stone-300 m-10 pb-3 rounded-md">
-        <div className="w-full h-12 bg-teal-300 flex items-center justify-center text-xl rounded-t-md">
+      <div className="border-solid border-2 border-gray-500 m-10 p-3 rounded-md">
+        {/* <div className="w-full h-12 bg-teal-300 flex items-center justify-center text-xl rounded-t-md">
           やること一覧
-        </div>
-        <div className="m-5 bg-stone-100 drop-shadow-md w-60 h-12 flex items-center justify-center text-xl">
-          掃除
-        </div>
-        <div className="m-5 bg-stone-100 drop-shadow-md w-60 h-12 flex items-center justify-center text-xl">
-          掃除
-        </div>
-        <div className="m-5 bg-stone-100 drop-shadow-md w-60 h-12 flex items-center justify-center text-xl">
-          掃除
+        </div> */}
+        <div className="flex items-center">
+          <div className="m-5 bg-stone-100 drop-shadow-md w-60 h-12 flex items-center justify-center text-xl">
+            掃除
+          </div>
+          <div>
+            <Pencil />
+          </div>
+          <div className="ml-3">
+            <TrashBox />
+          </div>
         </div>
       </div>
     </>
